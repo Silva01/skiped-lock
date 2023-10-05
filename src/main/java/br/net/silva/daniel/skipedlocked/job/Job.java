411a095084e299ffe0c3f21f03a8c87fa3318085
silva01 <silva20102@gmail.com>
2023-10-05 20:06:52 -0300

    Added print of dialect

--- src/main/java/br/net/silva/daniel/skipedlocked/job/Job.java
+++ src/main/java/br/net/silva/daniel/skipedlocked/job/Job.java
@@ -6,6 +6,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.LockModeType;
 import org.hibernate.LockMode;
 import org.hibernate.LockOptions;
+import org.hibernate.Session;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -30,6 +33,13 @@ public class Job {
     @Scheduled(cron = "0/3 * * * * *")
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void execute() throws InterruptedException {
+
+        /* Obter o dialect do hibernate */
+        final Session session = (Session) entityManager.getDelegate();
+        final SessionFactoryImpl sessionFactory = (SessionFactoryImpl) session.getSessionFactory();
+        final Dialect dialect = sessionFactory.getJdbcServices().getDialect();
+        System.out.println("Dialect: " + dialect);
+
         System.out.println("Job executed!");
         List<User> usuarios = repository.findByStatus(true, PageRequest.of(0, 1));
         User user = usuarios.get(0);
