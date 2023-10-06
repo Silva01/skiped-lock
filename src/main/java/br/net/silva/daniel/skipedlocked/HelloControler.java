8f1d76f9147df529b44a42ea9b20ca5a8a242ba6
silva01 <silva20102@gmail.com>
2023-10-06 19:52:23 -0300

    removed class not used

--- src/main/java/br/net/silva/daniel/skipedlocked/HelloControler.java
+++ src/main/java/br/net/silva/daniel/skipedlocked/HelloControler.java
@@ -3,7 +3,6 @@ package br.net.silva.daniel.skipedlocked;
 import br.net.silva.daniel.skipedlocked.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
